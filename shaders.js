15b6fc5d0bb57eb5965288c8cd4540451be9a280
RobuxgamingGamer <8wfq7hpy4z@privaterelay.appleid.com>
2026-02-20 22:58:25 +0200

    Update shaders.js

--- shaders.js
+++ shaders.js
@@ -1,33 +1,16 @@
 const vertexShaderSource = `#version 300 es
-precision highp float;
-
-in vec3 aPosition;
-uniform mat4 uProjection;
+in vec3 aPos;
+uniform mat4 uProj;
 uniform mat4 uView;
-uniform mat4 uModel;
-
 void main(){
-gl_Position = uProjection * uView * uModel * vec4(aPosition,1.0);
+gl_Position = uProj * uView * vec4(aPos,1.0);
 }
 `;
 
 const fragmentShaderSource = `#version 300 es
 precision highp float;
-
-uniform float uAlpha;
-uniform bool uGlossy;
-
 out vec4 outColor;
-
 void main(){
-
-vec3 baseColor = vec3(0.2,0.8,1.0);
-
-if(uGlossy){
-float light = dot(normalize(vec3(0.5,1.0,0.3)), vec3(0.0,0.0,1.0));
-baseColor += light * 0.3;
-}
-
-outColor = vec4(baseColor, uAlpha);
+outColor = vec4(0.2,0.8,1.0,1.0);
 }
 `;
