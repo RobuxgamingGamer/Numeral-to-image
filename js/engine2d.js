0b1a951aa0ad02dd0653d719b1e136b237ad95fb
RobuxgamingGamer <8wfq7hpy4z@privaterelay.appleid.com>
2026-02-21 15:56:22 +0200

    Update engine2d.js

--- js/engine2d.js
+++ js/engine2d.js
@@ -1,53 +1,60 @@
 const canvas=document.getElementById("canvas2D");
 const ctx=canvas.getContext("2d");
-const stats=document.getElementById("stats2D");
+const stats2D=document.getElementById("stats2D");
 
-function renderImage(){
+function generate2D(){
+let size=document.getElementById("size2D").value.split("-");
+let w=parseInt(size[0]);
+let h=parseInt(size[1]);
+let total=w*h;
+
+let mode=document.getElementById("mode2DSelect").value;
+
+let data=[];
+
+for(let i=0;i<total;i++){
+if(mode==="stress"){
+data.push(
+Math.floor(Math.random()*256),
+Math.floor(Math.random()*256),
+Math.floor(Math.random()*256)
+);
+}else{
+data.push(0,255,0);
+}
+}
+
+document.getElementById("input2D").value=data.join(" ");
+}
+
+function convert2D(){
 
 let start=performance.now();
 
-let size=document.getElementById("size").value.split("-");
+let size=document.getElementById("size2D").value.split("-");
 let w=parseInt(size[0]);
 let h=parseInt(size[1]);
 
 canvas.width=w;
 canvas.height=h;
 
-let imgData=ctx.createImageData(w,h);
-let values=document.getElementById("inputData")
+let img=ctx.createImageData(w,h);
+let values=document.getElementById("input2D")
 .value.trim().split(/\s+/);
 
-let format=document.getElementById("format").value;
-
 for(let i=0;i<w*h;i++){
-
-let r,g,b;
-
-if(format==="colorcode"){
-let hex=values[i].replace("#","");
-r=parseInt(hex.substring(0,2),16);
-g=parseInt(hex.substring(2,4),16);
-b=parseInt(hex.substring(4,6),16);
-}else{
-r=parseInt(values[i*3]);
-g=parseInt(values[i*3+1]);
-b=parseInt(values[i*3+2]);
+img.data[i*4]=parseInt(values[i*3]);
+img.data[i*4+1]=parseInt(values[i*3+1]);
+img.data[i*4+2]=parseInt(values[i*3+2]);
+img.data[i*4+3]=255;
 }
 
-imgData.data[i*4]=r;
-imgData.data[i*4+1]=g;
-imgData.data[i*4+2]=b;
-imgData.data[i*4+3]=255;
-}
-
-ctx.putImageData(imgData,0,0);
-
+ctx.putImageData(img,0,0);
 canvas.style.width="350px";
 canvas.style.height="350px";
 
-let end=performance.now();
-let ms=(end-start).toFixed(2);
+let ms=(performance.now()-start).toFixed(2);
 let fps=(1000/ms).toFixed(1);
 
-stats.textContent="Render: "+ms+" ms | FPS: "+fps;
+stats2D.textContent="Render: "+ms+" ms | FPS: "+fps;
 }
